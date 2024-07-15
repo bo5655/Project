8e75ce8e1ed282873cc86af4cef29f43fd70d106
bo5655 <1332343281@qq.com>
2024-07-16 07:35:04 +0800

    scrollview_modify fix

--- swm_demo/scrollview_modify.tsx
+++ swm_demo/scrollview_modify.tsx
@@ -8,3 +8,30 @@ return (
   </NativeDirectionalScrollView>
 );
 
+// new modify code
+const refreshControl = this.props.refreshControl;
+const scrollViewRef = this._scrollView.getForwardingRef(this.props.scrollViewRef);
+
+if (refreshControl) {
+  // RNOH: patch - use Android implementation
+  const { outer, inner } = splitLayoutProps(flattenStyle(props.style));
+  return (
+    <NativeDirectionalScrollView
+      {...props}
+      style={StyleSheet.compose(baseStyle, inner)}
+      ref={scrollViewRef}
+    >
+      {React.cloneElement(
+        refreshControl,
+        { style: StyleSheet.compose(baseStyle, outer) }
+      )}
+      {contentContainer}
+    </NativeDirectionalScrollView>
+  );
+}
+
+return (
+  <NativeDirectionalScrollView {...props} ref={scrollViewRef}>
+    {contentContainer}
+  </NativeDirectionalScrollView>
+);
